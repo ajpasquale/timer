58a100b141b3c489bf71a0ccb989ccdee4e742f1
Andrew Pasquale <pasquale.photoj@gmail.com>
2019-04-06 21:38:03 -0400

    added some easy tests

--- timer_test.go
+++ timer_test.go
@@ -1,16 +1,40 @@
 package timer
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestNewTimer(t *testing.T) {
-	tr := NewTimer(5, 550, true)
-	for {
-		if ok := tr.Tick(); ok {
-			fmt.Println("tick")
-			break
+	timer := NewTimer(1)
+	if timer == nil {
+		t.Fatal("timer should not be nil")
+	}
+}
+
+func TestTick(t *testing.T) {
+	timer := NewTimer(0)
+	if i, ok := timer.Tick(); ok {
+		if i != timer.ticks {
+			t.Fatal("tick method should return ticks")
 		}
+	} else {
+		t.Fatal("timer should have ticked")
+	}
+}
+
+func TestStop(t *testing.T) {
+	timer := NewTimer(0)
+	timer.Stop()
+	if _, ok := timer.Tick(); ok {
+		t.Fatal("timer should not have ticked")
+	}
+}
+
+func TestStart(t *testing.T) {
+	timer := NewTimer(0)
+	timer.Stop()
+	timer.Start()
+	if _, ok := timer.Tick(); !ok {
+		t.Fatal("timer should have ticked")
 	}
 }
